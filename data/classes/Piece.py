6ca679b5f584a6314fe1f2397aa36fdc99569fca
Even1ngshade <ethanmartyr01@gmail.com>
2025-03-25 16:05:12 +0000

    improving pawn promotion
    adding the ability to choose what piece you want the pawn to promote to

--- data/classes/Piece.py
+++ data/classes/Piece.py
@@ -38,16 +38,84 @@ class Piece:
         return False
 
     def handle_special_moves(self, board, prev_square, square):
-        if self.notation == ' ' and (self.y == 0 or self.y == 7):
-            from data.classes.pieces.Queen import Queen
-            square.occupying_piece = Queen((self.x, self.y), self.color, board)
+        # Pawn Promotion
+        if self.notation == ' ' and (self.y == 0 or self.y == 7):  # Pawn reaches last rank
+            promotion_piece = self.choose_promotion(board)
+            square.occupying_piece = promotion_piece  # Replace pawn with chosen piece
+        # Castling
         if self.notation == 'K':
-            if prev_square.x - self.x == 2:
+            if prev_square.x - self.x == 2:  # Queenside castling
                 rook = board.get_piece_from_pos((0, self.y))
                 rook.move(board, board.get_square_from_pos((3, self.y)), force=True)
-            elif prev_square.x - self.x == -2:
+            elif prev_square.x - self.x == -2:  # Kingside castling
                 rook = board.get_piece_from_pos((7, self.y))
                 rook.move(board, board.get_square_from_pos((5, self.y)), force=True)
+
+    def choose_promotion(self, board):
+        """ Opens a popup allowing the player to select a promotion piece. """
+        import pygame
+        from data.classes.pieces.Queen import Queen
+        from data.classes.pieces.Rook import Rook
+        from data.classes.pieces.Bishop import Bishop
+        from data.classes.pieces.Knight import Knight
+
+        screen = pygame.display.get_surface()
+        if screen is None:
+            raise RuntimeError("No active Pygame display surface found!")
+
+        font = pygame.font.Font(None, 40)
+
+        WHITE = (255, 255, 255)
+        BLACK = (0, 0, 0)
+        BUTTON_COLOR = (50, 150, 255)
+        BUTTON_HOVER = (30, 130, 230)
+
+        # Define button positions
+        button_width = 100
+        button_height = 50
+        x_center = screen.get_width() // 2
+        y_start = screen.get_height() // 2 - 100
+
+        pieces = [("Queen", Queen), ("Rook", Rook), ("Bishop", Bishop), ("Knight", Knight)]
+        buttons = []
+        
+        for i, (name, piece_class) in enumerate(pieces):
+            rect = pygame.Rect(x_center - 50, y_start + i * 60, button_width, button_height)
+            buttons.append((name, piece_class, rect))
+
+        selected_piece = None
+
+        while selected_piece is None:
+            screen.fill(WHITE)
+            text = font.render("Choose a piece:", True, BLACK)
+            screen.blit(text, (x_center - text.get_width() // 2, y_start - 60))
+
+            mouse_pos = pygame.mouse.get_pos()
+
+            for name, piece_class, rect in buttons:
+                color = BUTTON_HOVER if rect.collidepoint(mouse_pos) else BUTTON_COLOR
+                pygame.draw.rect(screen, color, rect, border_radius=10)
+                text_surf = font.render(name, True, WHITE)
+                text_rect = text_surf.get_rect(center=rect.center)
+                screen.blit(text_surf, text_rect)
+
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    for name, piece_class, rect in buttons:
+                        if rect.collidepoint(event.pos):
+                            selected_piece = piece_class((self.x, self.y), self.color, board)
+                            break
+
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        return Queen((self.x, self.y), self.color, board)  
+
+        return selected_piece
 
     def attacking_squares(self, board):
         return self.get_moves(board)
